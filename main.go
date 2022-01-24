e05121fa3a1340179386f8f00dc647422b7aeec1
Gartenschlaeger <mail@kaisnet.de>
2022-01-24 12:22:43 +0100

    initial version

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+)
+
+func staticContentHandler(response http.ResponseWriter, request *http.Request) {
+	path := path.Join("wwwroot", request.URL.Path[1:])
+	fmt.Printf("staticContentHandler path %v\n", path)
+
+	http.ServeFile(response, request, path)
+}
+
+func templateHandler(response http.ResponseWriter, request *http.Request) {
+	model := IndexModel{
+		Title: "Test"}
+
+	template, err := template.ParseFiles("wwwroot/index.html")
+	must(err)
+
+	template.Execute(response, model)
+}
+
+func pingHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "PONG")
+}
+
+func main() {
+	http.HandleFunc("/", staticContentHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/template", templateHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ models.go
@@ -0,0 +1,5 @@
+package main
+
+type IndexModel struct {
+	Title string
+}
